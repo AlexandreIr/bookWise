dc67f4a3256d1ab07f064f6594e8ece6aca38133
Alexandre Fernandes <alex.silva250@hotmail.com>
2025-01-31 23:43:54 -0300

    feat: globals and layout updated

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+
+const layout = ({children}:{children:ReactNode}) => {
+  return (
+    <main className="root-container">
+        <div className="mx-auto max-w-7xl">
+            HEADER
+
+            <div className="mt-20 pb-20">
+                {children}
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default layout
