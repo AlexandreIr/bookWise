d68caf17dcf394d584a1e1f41ac5454d5b220728
Alexandre Fernandes <alex.silva250@hotmail.com>
2025-02-02 20:50:15 -0300

    feat: types created

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  totalCopies: number;
+  availableCopies: number;
+  description: string;
+  color: string;
+  coverUrl: string;
+  video: string;
+  sumary: string;
+  isLoanedBook?: boolean;
+}
+
+interface BookCover {
+  variant?: BookCoverVariant
+  className?: string 
+  coverColor: string 
+  coverUrl: string
+}
+
+interface BookList {
+  title: string
+  books: Book[]
+  containerClass?: string
+}
